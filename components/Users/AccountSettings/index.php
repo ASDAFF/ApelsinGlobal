a363cf34f49281e374b6ce005ccc7fcfed32277a
Olga Rjabchikova <olga.rjabchicova@gmail.com>
2015-01-27 15:47:20 +0300

    Админка - изменение пароля

--- components/Users/AccountSettings/index.php
+++ components/Users/AccountSettings/index.php
@@ -11,6 +11,9 @@ if(isset($params[0])) {
         echo $accountSettings->getChangeAvatar();
     } else if($params[0]=='data') {
         echo $accountSettings->getChangeUserDataForm();
+    } else if($params[0]=='change_password') {
+        echo $accountSettings->getChangePasswordForm();
+//        echo 'тут вызываем функцию для обработки изменения паролья';
     }
     echo '<br><a href="'.$urlHelper->pageUrl('account_settings', null).'"><input type="button" value="К настрйокам"></a><br>';
 } else {
@@ -18,7 +21,9 @@ if(isset($params[0])) {
     $urlParam[0]="avatar";
     echo '<a href="'.$urlHelper->pageUrl('account_settings', $urlParam).'"><input type="button" value="Изменить аватар"></a>';
     $urlParam[0]="data";
-    echo '<a href="'.$urlHelper->pageUrl('account_settings', $urlParam).'"><input type="button" value="Изменить данные"></a><br>';
+    echo '<a href="'.$urlHelper->pageUrl('account_settings', $urlParam).'"><input type="button" value="Изменить данные"></a>';
+    $urlParam[0]="change_password";
+    echo '<a href="'.$urlHelper->pageUrl('account_settings', $urlParam).'"><input type="button" value="Изменить пароль"></a><br>';
 }
 ?>
 </center>
